e25d9e815288d83de9b8344cde1f239b3b9f1960
Pranav Teegavarapu <pranavnt@outlook.com>
2021-03-12 03:16:18 +0000

    prettify

--- main.py
+++ main.py
@@ -4,29 +4,39 @@ from replit import db
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+
 @client.event
 async def on_message(message):
-  if message.author == "client.user":
-    return
-  if "$" in (message.content)[0]:
-    if (message.content[1:5]=="give"):
-      if str(message.author) == "Pranav#0828":
-          try:
-            db[message.content[6:]] += 10
-          except:
-            db[message.content[6:]] = 10
-          # db[message.content[6:]] = db[message.content[6:]] + 10
-          await message.channel.send(str(message.content[6:])+" has a balance of "+str(db[message.content[6:]])+" brownie points.")
-      else:
-        await message.channel.send("You are not authorized to give brownie points")
-    elif message.content[1:5]=="":
-      pass
-    #elif "harish" in message.content.lower():
-    #    print("hi")
-    #    message.channel.send()
+    if message.author == "client.user":
+        return
+    if "$" in (message.content)[0]:
+        if (message.content[1:5] == "give"):
+            if str(message.author) == "Pranav#0828":
+                try:
+                    db[message.content[6:]] += 10
+                except:
+                    db[message.content[6:]] = 10
+                # db[message.content[6:]] = db[message.content[6:]] + 10
+                await message.channel.send(
+                    str(message.content[6:]) + " has a balance of " +
+                    str(db[message.content[6:]]) + " brownie points.")
+            else:
+                await message.channel.send(
+                    "You are not authorized to give brownie points")
+        elif message.content[1:6] == "points":
+            await message.channel.send(message.content[7:] +
+                                       " has a balance of " +
+                                       db[message.content[7:]] +
+                                       " brownie points")
+            pass
+        #elif "harish" in message.content.lower():
+        #    print("hi")
+        #    message.channel.send()
+
 
 client.run(os.getenv("TOKEN"))
