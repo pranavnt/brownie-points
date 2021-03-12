19c83db68f5d92251210983a9f09c986f09f7224
Pranav Teegavarapu <pranavnt@outlook.com>
2021-03-12 03:13:44 +0000

    only i can give brownie points

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+from replit import db
 
 client = discord.Client()
 
@@ -12,17 +13,18 @@ async def on_message(message):
   if message.author == "client.user":
     return
   if "$" in (message.content)[0]:
-    if str(message.author) == "Pranav#0828":
-      if (message.content[1:5]=="give"):
-        
-        await message.channel.send(message.content[6:])
+    if (message.content[1:5]=="give"):
+      if str(message.author) == "Pranav#0828":
+          try:
+            db[message.content[6:]] += 10
+          except:
+            db[message.content[6:]] = 10
+          # db[message.content[6:]] = db[message.content[6:]] + 10
+          await message.channel.send(str(message.content[6:])+" has a balance of "+str(db[message.content[6:]])+" brownie points.")
       else:
-        await message.channel.send(":/")
-        
-    else:
-      await message.channel.send("You are not authorized to give brownie points")
-      print(message.author)
-    
+        await message.channel.send("You are not authorized to give brownie points")
+    elif message.content[1:5]=="":
+      pass
     #elif "harish" in message.content.lower():
     #    print("hi")
     #    message.channel.send()
